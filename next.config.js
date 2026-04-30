e6aac42dff6bd4cdc54d034cf3270bb33e2b3694
leroiarnoldd <leroiarnoldd@gmail.com>
2026-04-30 10:21:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: ['localhost'],
+    unoptimized: true,
   },
 }
 
